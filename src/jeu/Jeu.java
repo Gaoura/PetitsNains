ddcf611809638c7f6b47e3e864762b66eb901be3
tazen2u <tazen2u@mim-e208-11.mim.ad.univ-lorraine.fr>
2016-05-11 10:41:36 +0200

    Fin des classes Jeu et Piece

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -14,6 +14,26 @@ public class Jeu {
 	public Joueur[] getJoueur() {
 		return this.joueur;
 	}
+
+	public Plateau getPlateau() {
+		return plateau;
+	}
+
+	public void setPlateau(Plateau plateau) {
+		this.plateau = plateau;
+	}
+
+	public int getJoueurCourant() {
+		return joueurCourant;
+	}
+
+	public void setJoueurCourant(int joueurCourant) {
+		this.joueurCourant = joueurCourant;
+	}
+
+	public void setJoueur(Joueur[] joueur) {
+		this.joueur = joueur;
+	}
 	
 	
 }
--- src/jeu/Piece.java
+++ src/jeu/Piece.java
@@ -5,7 +5,35 @@ public class Piece {
 	private String nom;
 	private Case pos;
 	
-	
+	public Piece (Joueur j, String nom, Case pos) {
+		this.setProprietaire(j);
+		this.setNom(nom);
+		this.setPos(pos);
+	}
+
+	public Joueur getProprietaire() {
+		return proprietaire;
+	}
+
+	public void setProprietaire(Joueur proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Case getPos() {
+		return pos;
+	}
+
+	public void setPos(Case pos) {
+		this.pos = pos;
+	}
 	
 	
 }
