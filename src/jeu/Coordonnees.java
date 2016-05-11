82065ee673283d27f219a92414b3df1a2e141ea9
Gaoura <gaoura@outlook.com>
2016-05-11 10:25:37 +0200

    Package jeu

--- src/jeu/Coordonnees.java
+++ src/jeu/Coordonnees.java
@@ -1,9 +1,33 @@
 package jeu;
 
-public class Coordonnees {
-
-	public Coordonnees() {
-		// TODO Auto-generated constructor stub
+public class Coordonnees
+{
+	private int abscisse;
+	private int ordonnee;
+	
+	public Coordonnees(int x, int y)
+	{
+		this.abscisse = x;
+		this.ordonnee = y;
 	}
 
+	public int getAbscisse()
+	{
+		return this.abscisse;
+	}
+
+	public int getOrdonnee()
+	{
+		return this.ordonnee;
+	}
+
+	public void setAbscisse(int abscisse)
+	{
+		this.abscisse = abscisse;
+	}
+
+	public void setOrdonnee(int ordonnee)
+	{
+		this.ordonnee = ordonnee;
+	}
 }
