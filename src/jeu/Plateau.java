c606c2c7b7b5a495f681785a22ad0bb09768d16e
Gaoura <gaoura@outlook.com>
2016-05-11 10:45:43 +0200

    Fin des modifs de la classe Plateau

--- src/jeu/Plateau.java
+++ src/jeu/Plateau.java
@@ -1,9 +1,30 @@
 package jeu;
 
-public class Plateau {
-
-	public Plateau() {
-		// TODO Auto-generated constructor stub
+public class Plateau
+{
+	private Case[][] cases;
+	
+	public Plateau(Case[][] c)
+	{
+		this.setCases(c);
+	}
+	
+	public Plateau(Coordonnees c)
+	{
+		Case[][] plateau = new Case[c.getAbscisse()][c.getOrdonnee()];
+		this.setCases(plateau);
 	}
 
+	public Case[][] getCases()
+	{
+		return this.cases;
+	}
+
+	public void setCases(Case[][] cases)
+	{
+		if (cases == null)
+			throw new NullPointerException();
+		
+		this.cases = cases;
+	}
 }
