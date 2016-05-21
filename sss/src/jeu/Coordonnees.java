aff18478a3b99b6405a381517157e9fd24898c4b
Sca <Sca@192.168.0.11>
2016-05-21 13:35:12 +0200

    Test commit

---
+++ sss/src/jeu/Coordonnees.java
@@ -0,0 +1,39 @@
+package jeu;
+
+public class Coordonnees
+{
+	private int abscisse;
+	private int ordonnee;
+	
+	public Coordonnees(int x, int y)
+	{
+		this.setAbscisse(x);
+		this.setOrdonnee(y);
+	}
+
+	public int getAbscisse()
+	{
+		return this.abscisse;
+	}
+
+	public int getOrdonnee()
+	{
+		return this.ordonnee;
+	}
+
+	public void setAbscisse(int abscisse)
+	{
+		if (abscisse < 0)
+			throw new IllegalArgumentException();
+		
+		this.abscisse = abscisse;
+	}
+
+	public void setOrdonnee(int ordonnee)
+	{
+		if (ordonnee < 0)
+			throw new IllegalArgumentException();
+		
+		this.ordonnee = ordonnee;
+	}
+}
