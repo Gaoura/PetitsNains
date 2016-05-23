062892190ce00a7ca719b988cb9b97fd52497563
Sca <Sca@192.168.0.11>
2016-05-23 18:31:56 +0200

    Exceptions
    Ajout des exceptions pour gérer les cases déjà occuppées et le cheval
    introuvable

---
+++ src/jeu/CaseDepartDejaOccupeeException.java
@@ -0,0 +1,10 @@
+package jeu;
+
+@SuppressWarnings("serial")
+public class CaseDepartDejaOccupeeException extends Exception
+{
+	public CaseDepartDejaOccupeeException()
+	{
+		super();
+	}
+}
---
+++ src/jeu/ChevalException.java
@@ -0,0 +1,10 @@
+package jeu;
+
+@SuppressWarnings("serial")
+public class ChevalException extends Exception
+{
+	public ChevalException(String msg)
+	{
+		super(msg);
+	}
+}
