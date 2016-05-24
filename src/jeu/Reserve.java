58733c451ede5e8450b3f1f460f4d58b11b876f4
tazen2u <tazen2u@mim-e216-07.mim.ad.univ-lorraine.fr>
2016-05-24 17:08:14 +0200

    Reserve
    mise à jour reserve

--- src/jeu/Reserve.java
+++ src/jeu/Reserve.java
@@ -1,6 +1,6 @@
 package jeu;
 
 public abstract class Reserve {
-	public abstract void sortirPiece();
+	public abstract void sortirPiece(Piece p) throws EcurieException;
 	public abstract void rentrerPiece(Piece p);
 }
