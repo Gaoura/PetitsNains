ad3614f8523a8e5b69beb1a7c3db8d96405720ba
tazen2u <tazen2u@mim-e216-07.mim.ad.univ-lorraine.fr>
2016-05-24 17:07:20 +0200

    Tour

--- src/jeu/Tour.java
+++ src/jeu/Tour.java
@@ -4,6 +4,4 @@ public interface Tour {
 	public void terminerTour();
 	public int lancerDe();
 	public void deplacerPiece(Piece p, int score);
-	public void rentrerCheval(Piece p) throws ChevalException, EcurieException, CaseDepartDejaOccupeeException;
-	public void sortirCheval(Piece p) throws EcurieException;
 }
