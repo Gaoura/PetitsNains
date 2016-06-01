66c2d031b37c4859a03cc25098ccf29c3ffe1ef0
Gaoura <gaoura@outlook.com>
2016-06-01 22:06:40 +0200

    Commentaires enlevés

--- src/jeu/Ecurie.java
+++ src/jeu/Ecurie.java
@@ -1,61 +1,88 @@
 package jeu;
 
-public class Ecurie extends Reserve {
-	// Nombre de chevaux pouvant encore entrer en jeu 
+public class Ecurie extends Reserve
+{
+	// Nombre de chevaux pouvant encore entrer en jeu
 	private int depart;
-	// Nombre de chevaux à avoir fini leur tour de plateau
+	// Nombre de chevaux a avoir fini leur tour de plateau
 	private int arrivee;
-	
-	public Ecurie() {
-		//Il faudrait mettre le nombre de chevaux en paramètres
-		//pour gérer un jeu à n chevaux
-		this.depart = 4;
-		this.arrivee = 4;
+
+	public Ecurie(int n)
+  {
+		this.setDepart(n);
+		this.setArrivee(0);
 	}
 
-	public int getDepart() {
+  /*/////////////////////////////////////////////////////////////////////////////////////////
+              #####                               #     #####
+             #     # ###### #####  ####          #     #     # ###### #####  ####
+             #       #        #   #             #      #       #        #   #
+             #  #### #####    #    ####        #        #####  #####    #    ####
+             #     # #        #        #      #              # #        #        #
+             #     # #        #   #    #     #         #     # #        #   #    #
+              #####  ######   #    ####     #           #####  ######   #    ####
+  *//////////////////////////////////////////////////////////////////////////////////////////
+
+	public int getDepart()
+  {
 		return this.depart;
 	}
 
-	public int getArrivee() {
+	public int getArrivee()
+  {
 		return this.arrivee;
 	}
 
-	private void setDepart(int depart) throws EcurieException {
+	private void setDepart(int depart) throws EcurieException
+  {
 		if (depart < 0)
-			throw new EcurieException("Plus aucun cheval dans l'écurie");
-		
+			throw new EcurieException("Plus aucun cheval dans l'ecurie");
+
 		this.depart = depart;
 	}
 
-	private void setArrivee(int arrivee) {
+	private void setArrivee(int arrivee)
+  {
 		this.arrivee = arrivee;
 	}
-	
 
-	// Fait revenir un cheval dégagé par un adversaire à l'écurie de départ
-	public void sortirCheval() throws EcurieException {
+  /*/////////////////////////////////////////////////////////////////////////////////////////
+                     #     #
+                     ##   ## ###### ##### #    #  ####  #####  ######  ####
+                     # # # # #        #   #    # #    # #    # #      #
+                     #  #  # #####    #   ###### #    # #    # #####   ####
+                     #     # #        #   #    # #    # #    # #           #
+                     #     # #        #   #    # #    # #    # #      #    #
+                     #     # ######   #   #    #  ####  #####  ######  ####
+  *//////////////////////////////////////////////////////////////////////////////////////////
+
+	// Fait revenir un cheval degage par un adversaire a l'ecurie de depart
+	public void sortirCheval() throws EcurieException
+	{
 		this.setDepart(this.depart + 1);
 	}
-	
 
-	// Fait revenir un cheval à l'écurie d'arrivée après qu'il ait fini son tour de plateau
-	public void chevalArrivee() {
+	// Fait revenir un cheval a l'ecurie d'arrivee apres qu'il ait fini son tour de plateau
+	public void chevalArrivee()
+	{
 		this.setArrivee(this.arrivee + 1);
 	}
 
+	// Sortir une piece de l'ecurie
 	@Override
-	public void sortirPiece(Piece p) throws EcurieException {
-		// sortir une pièce de l'écurie
+	public void sortirPiece(Piece p) throws EcurieException
+	{
+
 		this.setDepart(this.depart - 1);
 	}
 
 	@Override
-	public void rentrerPiece(Piece p) {
-		// rentrer une pièce dans l'écurie
-		// dans le jeu en fonction de l'exception attrappée, on fait la fonction qui lui est liée
+	public void rentrerPiece(Piece p)
+  {
+		// rentrer une piece dans l'ecurie
+		// dans le jeu en fonction de l'exception attrapee, on fait la fonction qui lui est liee
 		p.getProprietaire().getPieces().remove(p);
 		((CaseNormale) p.getPos()).setOccupant(null);
 	}
-	
+
 }
