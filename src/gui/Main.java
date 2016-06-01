d29fbd4b6da6aad7d6d2714c13f984969301c686
Gaoura <gaoura@outlook.com>
2016-06-01 17:59:16 +0200

    Correfifs + Main pour tests

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,12 +1,47 @@
 package gui;
 
+import jeu.Ecurie;
+import jeu.Jeu;
+import jeu.Joueur;
+import jeu.Plateau;
+
 public class Main
 {
-	public static void main(String[] args)
-	{
-		
-		FenetrePrincipale fenetrePrincipale = new FenetrePrincipale("Jeu des petits chevaux");
-		fenetrePrincipale.setVisible(true);
-	}
+    public static void main(String[] args)
+    {        
+        //FenetrePrincipale fenetrePrincipale = new FenetrePrincipale("Jeu des petits chevaux");
+        //fenetrePrincipale.setVisible(true);
+    
+        Joueur[] joueurs = new Joueur[4];
+        for(int i = 0; i < 4; i++)
+            joueurs[i] = new Joueur("Joueur" + i, i, new Ecurie());
+        
+        //Piece cheval1 = new Piece(joueurs[0], "cheval1", ,)
+        /*
+        Jeu jeu = new Jeu(null, new Plateau(cases_plateau));*/
+        Jeu j = new Jeu(joueurs, null);
+        System.out.println("OUI");
+        System.out.println(j.getJoueurCourant().getNomJoueur() + "  " + j.getJoueurCourant().getNumeroJoueur() + "  " + j.getNumJoueurCourant());
+        System.out.println("NON");
+        //System.exit(-1);
+        j.terminerTour();
+        System.out.println(j.getJoueurCourant().getNomJoueur() + "  " + j.getJoueurCourant().getNumeroJoueur() + "  " + j.getNumJoueurCourant());
+        System.out.println("OUI");
+        int[] tab = new int[6];
+        for (int i = 0; i < 6; i++)
+            tab[i] = 0;
+        for(int i = 0; i < 10000; i++)
+        {
+            int k = j.lancerDe();
+        
+            tab[k-1] = tab[k-1] + 1;
+            //if (k < 1 || k > 6)
+                //System.out.println("NAN MAIS T'ES SERIEUX" + k);
+            
+        }
+        for (int i = 0; i < 6; i++)
+            System.out.println("Fréquence de " + (i+1) + ":" + tab[i]);
+        
+    }
 
 }
--- src/jeu/Ecurie.java
+++ src/jeu/Ecurie.java
@@ -49,54 +49,6 @@ public class Ecurie extends Reserve {
 		// sortir une pièce de l'écurie
 		this.setDepart(this.depart - 1);
 	}
-		/*//Générer une position de départ en fonction du joueur en fonction du numéro de joueur courant
-		// 0 = 0,6
-		// 1 = 8,0
-		// 2 = 14,8
-		// 3 = 6,14
-		//ensuite regarder dans la hashtable pour savoir sur quel pivot on démarre
-		Coordonnees c = null;
-		switch (this.getNumJoueurCourant()) {
-			case 0:
-				c = new Coordonnees(0,6);
-				break;
-			case 1 :
-				c = new Coordonnees(8,0);
-				break;
-			case 2 :
-				c = new Coordonnees(14,8);
-				break;
-			case 3 :
-				c = new Coordonnees(6,14);
-				break;
-		}
-		Piece occupantDepart = ((CaseNormale) this.getPlateau().getCaseDepuisCoordonnees(c)).getOccupant();
-		
-		if (occupantDepart == null) {
-			this.ajout (p);
-		} else if (occupantDepart.getProprietaire() != this.getJoueurCourant()) {
-			//Le cheval sur la case de départ retourne à l'écurie
-			this.rentrerPiece(occupantDepart);
-			ajout(p);
-		} else if (occupantDepart.getProprietaire() == this.getJoueurCourant()) 
-			throw new CaseDepartDejaOccupeeException();
-	}
-	
-	public void ajout (Piece p) throws ChevalException, EcurieException {
-		Piece piece = null;
-		for (Piece iterateurPiece : this.getJoueurCourant().getPieces()) {
-			if (iterateurPiece.getNom().equals(p.getNom())) {
-				piece = iterateurPiece;
-				break;
-			}
-		}
-		
-		if (p == null) throw new ChevalException("Cheval introuvable");
-		this.getJoueurCourant().getStock().sortirPiece();
-		String nomCheval = "cheval" + (4-((Ecurie) this.getJoueurCourant().getStock()).getDepart());
-		String newDir = ht.get(p.getPos().getPosition().toString().hashCode());
-		this.getJoueurCourant().add(new Piece(this.getJoueurCourant(), nomCheval, piece.getPos(), newDir));
-	}*/
 
 	@Override
 	public void rentrerPiece(Piece p) {
